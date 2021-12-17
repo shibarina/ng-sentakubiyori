4abff2b45c8bbeeecdc8d32446c0d4a1611ef0c8
morihara-y <yutaka.morihara@gmail.com>
2021-12-17 11:05:23 +0900

    refs #0 init pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { PagesComponent } from './pages/pages.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'pages', component: PagesComponent,
+  loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
